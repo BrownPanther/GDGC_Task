995f4a81be75290645b14dde373a556f70062d60
BrownPanther <47709472+BrownPanther@users.noreply.github.com>
2025-12-04 15:49:26 +0530

    Change API base URL to production

--- frontend/script.js
+++ frontend/script.js
@@ -16,7 +16,7 @@ if (splash) {
 }
 
 
-const API_BASE_URL = "http://localhost:5000";
+const API_BASE_URL = "https://gdgc-task.onrender.com";
 
 const searchInput = document.getElementById("searchInput");
 const roleFilter = document.getElementById("roleFilter");
